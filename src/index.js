14f92d8719b3b63ac59d9920c0c36be5dfbff8a3
vurles <vurles27@gmail.com>
2018-12-07 09:51:48 -0300

    proyecto completo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-/* Acá va tu código */
+document.getElementById('Encriptar').addEventListener('click', (event) => {
+    event.preventDefault();
+    let texto = document.getElementById('rescate').value;
+    let desplazo = document.getElementById('seguridad').value;
+    document.getElementById('nuevoTexto').innerHTML=window.cipher.encode(texto, desplazo);
+})
+
+
+document.getElementById('Descencriptar').addEventListener('click', (event) => {
+    event.preventDefault();
+    let cajaDeco = document.getElementById('rescate').value;
+    let desplazoDeco = document.getElementById('seguridad').value;
+    document.getElementById('nuevoTexto').innerHTML=window.cipher.decode(cajaDeco, desplazoDeco);
+
+
+
+})
